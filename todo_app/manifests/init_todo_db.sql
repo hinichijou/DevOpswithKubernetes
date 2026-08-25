106b6e2104682117299c9c402b232c585b9ba0cd
hinichijou <163578861+hinichijou@users.noreply.github.com>
2026-08-26 00:25:14 +0300

    Test 3.7 workflow for main branch

--- todo_app/manifests/init_todo_db.sql
+++ todo_app/manifests/init_todo_db.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS todos (
 );
 
 -- Can be used to add default entries to database
+-- 3.7: Test deployment to main branch with existing images
 -- INSERT INTO todos (title) VALUES ('Learn Kubernetes basics');
 -- INSERT INTO todos (title) VALUES ('Deploy application to cluster');
 -- INSERT INTO todos (title) VALUES ('Configure persistent volumes');
