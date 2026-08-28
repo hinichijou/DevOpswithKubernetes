eefdbe5d888ce3d941ab2ca14aecfc5b701d8d54
hinichijou <163578861+hinichijou@users.noreply.github.com>
2026-08-28 22:34:00 +0300

    Updates to db backup script

--- todo_app/db_backup_job/db_backup.sh
+++ todo_app/db_backup_job/db_backup.sh
@@ -1,9 +1,31 @@
 #!/usr/bin/env bash
 set -e
 
-if [ $POSTGRES_URL ] && [ $BUCKET_URL ] && [ $BACKUP_PATH ] && [ $POSTGRES_USER ] && [ $POSTGRES_PASSWORD ] && [ $POSTGRES_DB ]
+if [ $POSTGRES_URL ] && [ $BUCKET_URL ] && [ $BACKUP_FILE ] && [ $METADATA_URL ] && \
+  [ $BACKUP_PATH ] && [ $POSTGRES_USER ] && [ $POSTGRES_PASSWORD ] && [ $POSTGRES_DB ]
 then
-  pg_dump -v postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_URL/$POSTGRES_DB > $BACKUP_PATH
+  echo "Dumping database to $BACKUP_PATH/$BACKUP_FILE"
 
-  curl -F ‘data=@$BACKUP_PATH’ $BUCKET_URL
+  pg_dump -v "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_URL/$POSTGRES_DB" > "$BACKUP_PATH/$BACKUP_FILE"
+
+  if [ -f "$BACKUP_PATH/$BACKUP_FILE" ]
+    echo "Backup file created successfully."
+  else
+    echo "Backup file creation failed!"
+    # Makes sense to exit here but better to test that the script works in the first place first
+
+  # To avoid needing to install the complete gcloud api to use gcloud auth we need to request the metadata server for a access token
+  # https://docs.cloud.google.com/compute/docs/metadata/querying-metadata#obtain-oauth-tokens
+  # -q suppresses default wget output. -O - directs output to console.
+  RESPONSE=$(wget -q -O - \
+    --header="Authorization: Bearer $(wget -q -O - --header="Metadata-Flavor: Google" $METADATA_URL | jq -r '.access_token')" \
+    --header="Content-Type: application/octet-stream" \
+    --post-file="$BACKUP_PATH/$BACKUP_FILE" "$BUCKET_URL/o?uploadType=media&name=$BACKUP_FILE")
+
+  # $? holds the exit status of most recently executed command. 0 means success.
+  if [ $? -eq 0 ]; then
+    echo "Backup upload successful."
+  else
+    echo "Backup upload failed! Reason: $RESPONSE"
+  fi
 fi
