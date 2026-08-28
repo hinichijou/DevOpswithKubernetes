6dcd1241bcc8b618d9b94cbc5217b0d8a4239625
hinichijou <163578861+hinichijou@users.noreply.github.com>
2026-08-29 01:41:29 +0300

    Correct the db backup job run condition

--- todo_app/db_backup_job/db_backup.sh
+++ todo_app/db_backup_job/db_backup.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ $POSTGRES_SVC ] && [ $POSTGRES_PORT ] && [ $BUCKET_URL ] && [ $BACKUP_FILE ] && [ $METADATA_URL ] && \
-  [ $BACKUP_PATH ] && [ $POSTGRES_USER ] && [ $POSTGRES_PASSWORD ] && [ $POSTGRES_DB ]
+  [ $BACKUP_PATH ] && [ $POSTGRES_USER ] && [ $PGPASSWORD ] && [ $POSTGRES_DB ]
 then
   FILENAME="${NAMESPACE}-$(date +%s)-${BACKUP_FILE}"
   FILEPATH="${BACKUP_PATH}/${FILENAME}"
