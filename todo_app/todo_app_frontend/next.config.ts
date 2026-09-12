d643342522bcc1451f3e140de065bf7405b6c974
hinichijou <163578861+hinichijou@users.noreply.github.com>
2026-09-12 18:47:09 +0300

    Handle health probes with basePath

--- todo_app/todo_app_frontend/next.config.ts
+++ todo_app/todo_app_frontend/next.config.ts
@@ -4,7 +4,25 @@ const nextConfig: NextConfig = {
   cacheComponents: false,
   output: "standalone",
   // root path for the application. needs to be injected during the build if build env dependent
-  basePath: process.env.BASE_PATH ?? ''
+  basePath: process.env.BASE_PATH ?? '',
+
+  // Can define incoming requests (health probes) to redirect to the basepath https://nextjs.org/docs/app/api-reference/config/next-config-js/redirects
+  redirects() {
+    return [
+      {
+        source: '/health',
+        destination: `${process.env.BASE_PATH}/health`,
+        permanent: false,
+        basePath: false
+      },
+      {
+        source: '/ready',
+        destination: `${process.env.BASE_PATH}/ready`,
+        permanent: false,
+        basePath: false
+      },
+    ]
+  },
 };
 
 export default nextConfig;
